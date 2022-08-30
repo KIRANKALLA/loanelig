97f8631d03a28cf90018e6935fd6796e6151a046
KIRANKALLA <kallakiran1974@gmail.com>
2022-08-30 15:02:22 +0530

    Add files via upload

---
+++ LoanElig.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pickle
+
+pickle_in=open('LoanElig.pkl','rb')
+clf=pickle_in.load(pickle_in)
+
+st.markdown(unsafe_allow_html=True)
+st.title('LOAN ELIGIBILITY')
+
+a=st.select_input('Enter Gender',('Male','Female'))
+b=st.select_input('Martial Status',('Married','Unmarried'))
+c=st.number_input('Enter applicant income')
+d=st.number_input('Enter Loan amount')
+e=''
+if a=='Male':
+    a=0
+else:
+    a=1
+if b=='Married':
+    b=1
+else:
+    b=0
+e=clf.predict([[a,b,c,d]]).squeeze()
+if e==0:
+    st.success('Not Approved')
+else:
+    st.success('Approved')
+
